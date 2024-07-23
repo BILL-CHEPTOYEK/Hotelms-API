1f0ab59f8ccf667cbd075202d4ec56c2f1636aa4
Bill <bill@Trojan.horse>
2024-07-23 15:27:08 +0300

    room functionality

--- routes/hotel.routes.js
+++ routes/hotel.routes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const hotelController = require("../controllers/hotel.controller");
 
 // Room routes
+router.post("/rooms", hotelController.createRoom);
 router.get("/rooms", hotelController.getAllRooms);
+router.get("/rooms/:id", hotelController.getRoomById);
+router.put("/rooms/:id", hotelController.updateRoom);
+router.delete("/rooms/:id", hotelController.deleteRoom);
 
+
+router.get("/availability", hotelController.checkRoomAvailability);
 // User routes
 router.get("/users", hotelController.getAllUsers);
 router.post("/users", hotelController.addUser);
@@ -24,9 +30,12 @@ router.get("/reservations", hotelController.getAllReservations);
 router.get("/reservations/:id", hotelController.getReservationById);
 router.put("/reservations/:id", hotelController.updateReservation);
 router.delete("/reservations/:id", hotelController.deleteReservation);
-router.get("/availability", hotelController.checkRoomAvailability);
+
 
 // Analytics routes
 router.get("/analytics", hotelController.getAnalytics);
 
 module.exports = router;
+
+
+//http://localhost:5000/api/hotelms/
